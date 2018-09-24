b7d38096a11b6831719dd34598def03df8da8ced
Matt Zuzolo <mattzuzolo@outlook.com>
2018-09-24 11:23:28 -0400

    UPdated about page

--- src/components/containers/AboutContainer.js
+++ src/components/containers/AboutContainer.js
@@ -5,7 +5,7 @@ class AboutContainer extends Component {
     return(
       <div className="container div--about-container">
         <h1>OBRA was created by <a href="https://mattzuzolo.github.io/">Matt Zuzolo</a>.</h1>
-        <p>You can view the code on my <a href="https://www.github.com/mattzuzolo">GitHub page</a>, or directly at the <a href="https://github.com/mattzuzolo/obra-front-v2">repository</a>.</p>
+        <p>You can view the code on my <a href="https://www.github.com/mattzuzolo">GitHub page</a>, or directly at the <a href="https://github.com/mattzuzolo/obra">repository</a>.</p>
         <p className="source-credit">This project was made using data from <a href="https://www.harvardartmuseums.org/collections/api">The Harvard Museum API.</a></p>
       </div>
     );
