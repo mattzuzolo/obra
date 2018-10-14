f6f7f9a893ccec5c08a1bf2fa509fb7efda0b96e
Matt Zuzolo <mattzuzolo@outlook.com>
2018-10-14 15:40:51 -0400

    Commented out redux logs

--- src/index.js
+++ src/index.js
@@ -17,17 +17,17 @@ import reducer from './reducers/reducer'
 // const store = configureStore()
 let store = createStore(reducer);
 
-const addLoggingToDispatch = (store) => {
-  const rawDispatch = store.dispatch;
-  return (action) => {
-    console.group("DISPATCH");
-    console.log('%c state before dispatch', 'color: red', store.getState());
-    const returnValue = rawDispatch(action);
-    console.log('%c state after dispatch', 'color: blue', store.getState());
-    console.groupEnd();
-    return returnValue;
-  }
-}
+// const addLoggingToDispatch = (store) => {
+//   const rawDispatch = store.dispatch;
+//   return (action) => {
+//     console.group("DISPATCH");
+//     console.log('%c state before dispatch', 'color: red', store.getState());
+//     const returnValue = rawDispatch(action);
+//     console.log('%c state after dispatch', 'color: blue', store.getState());
+//     console.groupEnd();
+//     return returnValue;
+//   }
+// }
 
 store.dispatch = addLoggingToDispatch(store);
 
