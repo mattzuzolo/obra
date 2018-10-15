a8416be0a749a8e97de4c7d67d4ddda72bddd6a9
Matt Zuzolo <mattzuzolo@outlook.com>
2018-10-15 10:44:11 -0400

    Added type.js file and update reducer to use imported constants rather than string

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,3 +1,5 @@
+import { UPDATE_ARTWORK_ARRAY, SELECT_ARTWORK, LOGIN_USER, SELECT_ANNOTATION } from "./types"
+
 const defaultState = {
   artworkArray: [],
   selectedArtwork: {},
@@ -8,20 +10,18 @@ const defaultState = {
 export default function(state = defaultState, action) {
 
   switch(action.type){
-    case "UPDATE_ARTWORK_ARRAY":
-      return {...state, artworkArray: [...action.payload]}
+    case UPDATE_ARTWORK_ARRAY:
       // return {...state, artworkArray: [...state.artworkArray, ...action.payload]} //this maintains running list. NO DELETION
-    case "CHANGE_MESSAGE":
-      return {testString: action.payload}
+      return {...state, artworkArray: [...action.payload]}
 
-    case "SELECT_ARTWORK":
+    case SELECT_ARTWORK:
       return {...state, selectedArtwork: action.payload}
 
-    case "LOGIN_USER":
-      return {...state, loggedInUser: action.payload}
+    case SELECT_ANNOTATION:
+      return {...state, selectedAnnotation: action.payload}
 
-    case "SELECT_ANNOTATION":
-        return {...state, selectedAnnotation: action.payload}
+    case LOGIN_USER:
+      return {...state, loggedInUser: action.payload}
 
     default:
       return state
